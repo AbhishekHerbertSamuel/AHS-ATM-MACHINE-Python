bbac6a81ed3dc6af6e6c5ae7d6126440f78b135d
Abhishek Herbert Samuel <86236032+AbhishekHerbertSamuel@users.noreply.github.com>
2022-03-27 09:01:34 +0530

    Update Abhishek Herbert Samuel's program implementing basic ATM Machine functionality using Python.py

--- Abhishek Herbert Samuel's program implementing basic ATM Machine functionality using Python.py
+++ Abhishek Herbert Samuel's program implementing basic ATM Machine functionality using Python.py
@@ -20,14 +20,14 @@ def pin(atmpin):
        print("Option to Check Account Balance:")
        select()
 def select():
-    print("Which option would you like to select from the above two?Enter either of the above phrases exactly as given.")
-    x=input()
-    if(x=="Option to Withdraw an Amount:"):
+    print("Which option would you like to select from the above two?Enter 1 if you wish to withdraw and 2 if you wish to check your account balance.")
+    x=int(input())
+    if(x==1):
         withdraw()
-    elif(x=="Option to Check Account Balance:"):
+    elif(x==2):
         check()
     else:
-        print("Enter the above phrases with : as they are displayed.Please try again.")
+        print("Enter the numbers 1/2 only depending on what you wish to do.Please try again")
         sys.exit()
 def withdraw():
     balance=200000
@@ -35,13 +35,16 @@ def withdraw():
     if (y<100):
         print("The minimum amount that you can withdraw is 100 rupees.Please try again.")
         sys.exit()
+    if (y>balance):
+        print("You can't withdraw more money than your total balance. Please try again.")
+        sys.exit()
     balance=balance-y
     print("The remaining amount in your account after withdrawal of",y,"rupees is:",balance,"rupees")
     print("Thank you for using ABC Bank.Looking forward to seeing you again.")
 def check():
    balance=200000    
    print("Your account balance currently is:",balance,"rupees")
-   print("\n If you wish to withdraw money enter the phrase Yes below exactly as it is written:")
+   print("\n If you wish to withdraw money enter the phrase Yes below exactly as it is written:Else write No or press enter.")
    a=input()
    if(a=="Yes"):
        withdraw()
